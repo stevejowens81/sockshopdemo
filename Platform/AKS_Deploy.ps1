20bd9baf102ada7544bde7ae13a9aa7f376ea062
Steve Owens <steve.owens@bjss.com>
2018-04-17 14:21:21 +0100

    Update

--- Platform/AKS_Deploy.ps1
+++ Platform/AKS_Deploy.ps1
@@ -3,7 +3,7 @@ $ResourceGroup="ACS-RG"
 $Location="westeurope"
 $AKSClusterName="BJSS-DEMO-ACS"
 $AKSNamespace1="sock-shop"
-$AKSNamespace2="vote"
+$AKSNamespace2="monitoring"
 
 # Enable the Required Azure Resource Providers
 az provider register -n Microsoft.Network
@@ -20,7 +20,7 @@ az acs create --orchestrator-type kubernetes --resource-group $ResourceGroup --n
 #Install the Kubernetes Command Line Interface
 az acs kubernetes install-cli
 
-sleep 1200
+sleep 300
 
 #Connect to the Kubernetes Cluster
 az acs kubernetes get-credentials --resource-group=$ResourceGroup --name=$AKSClusterName
@@ -34,7 +34,36 @@ kubectl create namespace $AKSNamespace1
 kubectl create namespace $AKSNamespace2
 
 #Deploy SockShop Application
-kubectl apply -f https://bjssdemoacs.blob.core.windows.net/yaml/sockshop/microservices-demo/deploy/kubernetes/complete-demo.yaml
-kubectl apply -f https://bjssdemoacs.blob.core.windows.net/yaml/sockshop/microservices-demo/deploy/kubernetes/manifests-alerting/
-kubectl apply -f https://bjssdemoacs.blob.core.windows.net/yaml/sockshop/microservices-demo/deploy/kubernetes/manifests-monitoring/
-kubectl apply -f https://bjssdemoacs.blob.core.windows.net/yaml/sockshop/microservices-demo/deploy/kubernetes/manifests-logging
+kubectl apply -f https://github.com/stevejowens81/sockshopdemo/tree/master/microservices-demo/deploy/kubernetes/complete-demo.yaml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-alerting/alertmanager-configmap.yaml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-alerting/alertmanager-dep.yaml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-alerting/alertmanager-svc.yaml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/grafana-configmap.yaml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/grafana-dep.yaml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/grafana-import-dash-batch.yaml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/grafana-svc.yaml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/monitoring-ns.yaml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/prometheus-alertrules.yaml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/prometheus-configmap.yaml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/prometheus-cr.yml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/prometheus-crb.yml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/prometheus-dep.yaml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/prometheus-exporter-disk-usage-ds.yaml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/prometheus-exporter-kube-state-dep.yaml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/prometheus-exporter-kube-state-svc.yaml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/prometheus-sa.yml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/prometheus-svc.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-logging/elasticsearch.yml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-logging/fluentd-cr.yml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-logging/fluentd-crb.yml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-logging/fluentd-daemon.yml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-logging/fluentd-sa.yml
+kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-logging/kibana.yml
+
+
+steve@Azure:~$ kubectl apply -f https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/prometheus-cr.yml
+Error from server (Forbidden): error when creating "https://raw.githubusercontent.com/stevejowens81/sockshopdemo/master/microservices-demo/deploy/kubernetes/manifests-monitoring/prometheus-cr.yml": clusterroles.rbac.authorization.k8s.io "prometheus" is forbidden: attempt to grant extra privileges: [PolicyRule{Resources:["nodes"], APIGroups:[""], Verbs:["get"]} PolicyRule{Resources:["nodes"], APIGroups:[""], Verbs:["list"]} PolicyRule{Resources:["nodes"], APIGroups:[""], Verbs:["watch"]} PolicyRule{Resources:["nodes/proxy"], APIGroups:[""], Verbs:["get"]} PolicyRule{Resources:["nodes/proxy"], APIGroups:[""], Verbs:["list"]} PolicyRule{Resources:["nodes/proxy"], APIGroups:[""], Verbs:["watch"]} PolicyRule{Resources:["services"], APIGroups:[""], Verbs:["get"]} PolicyRule{Resources:["services"], APIGroups:[""], Verbs:["list"]} PolicyRule{Resources:["services"], APIGroups:[""], Verbs:["watch"]} PolicyRule{Resources:["endpoints"], APIGroups:[""], Verbs:["get"]} PolicyRule{Resources:["endpoints"], APIGroups:[""], Verbs:["list"]} PolicyRule{Resources:["endpoints"], APIGroups:[""], Verbs:["watch"]} PolicyRule{Resources:["pods"], APIGroups:[""], Verbs:["get"]} PolicyRule{Resources:["pods"], APIGroups:[""], Verbs:["list"]} PolicyRule{Resources:["pods"], APIGroups:[""], Verbs:["watch"]} PolicyRule{NonResourceURLs:["/metrics"], Verbs:["get"]}] user=&{kubeconfig  [system:authenticated] map[]} ownerrules=[] ruleResolutionErrors=[]
+
+
+
