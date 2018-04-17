d79f6c58d65e6efd58a44e1473a9ba9d17774c42
Steve Owens <steve.owens@bjss.com>
2018-04-17 11:17:49 +0100

    Latest

--- Platform/AKS_Deploy.sh
+++ Platform/AKS_Deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Set Deployment Variables
-ResourceGroup="SockShopDemo-RG"
-Location="uksouth"
-AKSClusterName="SockShopDemo-AKS"
-AKSNodes="3"
-
+ResourceGroup="ACS-RG"
+Location="westeurope"
+AKSClusterName="BJSS-DEMO-ACS"
+AKSNamespace1="sock-shop"
+AKSNamespace2="vote"
 
 # Enable the Required Azure Resource Providers
 az provider register -n Microsoft.Network
@@ -12,25 +12,30 @@ az provider register -n Microsoft.Storage
 az provider register -n Microsoft.Compute
 az provider register -n Microsoft.ContainerService
 
-
-
 # Create a Azure Resourge Group
-az group create \
-    --name $ResourceGroup \
-    --location $Location
+az group create --name $ResourceGroup --location $Location
 
-az aks create \
-    --resource-group $ResourceGroup \
-    --name $AKSClusterName \
-    --node-count $AKSNodes \
-    --generate-ssh-keys
+# Create the Kubernetes Cluster
+az acs create --orchestrator-type kubernetes --resource-group $ResourceGroup --name $AKSClusterName --generate-ssh-keys
 
-#Install the Kubernetes Command Line
-az aks install-cli
+#Install the Kubernetes Command Line Interface
+az acs kubernetes install-cli
+
+sleep 1200
 
 #Connect to the Kubernetes Cluster
-az aks get-credentials --resource-group $ResourceGroup --name $AKSClusterName
+az acs kubernetes get-credentials --resource-group=$ResourceGroup --name=$AKSClusterName
 
 # Return a list of Kubernetes Nodes
 nodes=$(kubectl get nodes)
 echo "$nodes"
+
+#Create the Kubernetes Namespace
+kubectl create namespace $AKSNamespace1
+kubectl create namespace $AKSNamespace2
+
+#Deploy SockShop Application
+kubectl apply -f https://bjssdemoacs.blob.core.windows.net/yaml/sockshop/microservices-demo/deploy/kubernetes/complete-demo.yaml
+kubectl apply -f https://bjssdemoacs.blob.core.windows.net/yaml/sockshop/microservices-demo/deploy/kubernetes/manifests-alerting/
+kubectl apply -f https://bjssdemoacs.blob.core.windows.net/yaml/sockshop/microservices-demo/deploy/kubernetes/manifests-monitoring/
+kubectl apply -f https://bjssdemoacs.blob.core.windows.net/yaml/sockshop/microservices-demo/deploy/kubernetes/manifests-logging
